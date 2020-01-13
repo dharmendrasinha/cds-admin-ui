0fad945ae48738eca5e23c82a0fa9d1b0d4d20bc
neeleshparihar <pariharneelesh@gmail.com>
2020-01-12 19:27:48 -0500

    Initial version of cdsadminui generated by JHipster-6.5.1

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import './vendor';
+import { CdsadminuiSharedModule } from 'app/shared/shared.module';
+import { CdsadminuiCoreModule } from 'app/core/core.module';
+import { CdsadminuiAppRoutingModule } from './app-routing.module';
+import { CdsadminuiHomeModule } from './home/home.module';
+import { CdsadminuiEntityModule } from './entities/entity.module';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+import { JhiMainComponent } from './layouts/main/main.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
+import { ErrorComponent } from './layouts/error/error.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CdsadminuiSharedModule,
+    CdsadminuiCoreModule,
+    CdsadminuiHomeModule,
+    // jhipster-needle-angular-add-module JHipster will add new module here
+    CdsadminuiEntityModule,
+    CdsadminuiAppRoutingModule
+  ],
+  declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent],
+  bootstrap: [JhiMainComponent]
+})
+export class CdsadminuiAppModule {}
